400daed756c3005a61d7265b636ed63361b01586
Mert <tokcan.mert@gmail.com>
2022-10-13 22:15:52 +0300

    ucuncu taskımı geri aldım

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,16 +2,13 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
-
+        System.out.println("merhaba ");
 
         for (int i=0; i<5; i++){
             System.out.println("Merhaba"+i);
         }
 
-        int sayac=0;
-        do {
-            System.out.println("yeni merhaba");
-        }while (sayac<5);
+
 
 
 
